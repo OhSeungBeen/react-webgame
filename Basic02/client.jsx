9d32d9b8e1fe96d00bdad59cd60877c41283a704
OhSeungBeen <osb1992@naver.com>
2021-07-19 17:12:39 +0900

    React Hooks useEffect

--- Basic02/client.jsx
+++ Basic02/client.jsx
@@ -6,10 +6,12 @@ import Step02 from './Step02';
 import Step03 from './Step03';
 import Step04 from './Step04';
 import Step04 from './Step05';
+import Step04 from './Step06';
 
 //ReactDOM.render(<Step01/>, document.querySelector("#root"));
 //ReactDOM.render(<Step02/>, document.querySelector("#root"));
 //ReactDOM.render(<Step03/>, document.querySelector("#root"));
 //ReactDOM.render(<Step04/>, document.querySelector("#root"));
-ReactDOM.render(<Step05/>, document.querySelector("#root"));
+//ReactDOM.render(<Step05/>, document.querySelector("#root"));
+ReactDOM.render(<Step06/>, document.querySelector("#root"));
 
