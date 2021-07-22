98d63223034e1a78b63b653ff78fc21c444f2d88
OhSeungBeen <osb1992@naver.com>
2021-07-22 23:04:05 +0900

    Practice

---
+++ Practice/client.jsx
@@ -0,0 +1,41 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const Practice01 = require('./Practice01');
+const Practice02 = require('./Practice02');
+import Practice03 from './Practice03';
+import Practice04 from './Practice04';
+import Practice05 from './Practice05';
+import Practice06 from './Practice06';
+import Practice07 from './Practice07';
+import Practice08 from './Practice08';
+import Practice09 from './Practice09';
+import Practice10 from './Practice10';
+import Practice11 from './Practice11';
+
+import Step02 from './Step02';
+import Step03 from './Step03';
+import Step04 from './Step04';
+import Step05 from './Step05';
+import Step06 from './Step06';
+import Step07 from './Step07';
+
+//ReactDOM.render(<Step02/>, document.querySelector("#root"));
+//ReactDOM.render(<Step03/>, document.querySelector("#root"));
+//ReactDOM.render(<Step04/>, document.querySelector("#root"));
+//ReactDOM.render(<Step05/>, document.querySelector("#root"));
+//ReactDOM.render(<Step06/>, document.querySelector("#root"));
+//ReactDOM.render(<Step07/>, document.querySelector("#root"));
+
+
+//ReactDOM.render(<Practice01/>, document.querySelector("#root"));
+//ReactDOM.render(<Practice02/>, document.querySelector("#root"));
+//ReactDOM.render(<Practice03/>, document.querySelector("#root"));
+//ReactDOM.render(<Practice04/>, document.querySelector("#root"));
+//ReactDOM.render(<Practice05/>, document.querySelector("#root"));
+//ReactDOM.render(<Practice06/>, document.querySelector("#root"));
+//ReactDOM.render(<Practice07/>, document.querySelector("#root"));
+//ReactDOM.render(<Practice08/>, document.querySelector("#root"));
+//ReactDOM.render(<Practice09/>, document.querySelector("#root"));
+//ReactDOM.render(<Practice10/>, document.querySelector("#root"));
+ReactDOM.render(<Practice11/>, document.querySelector("#root"));
